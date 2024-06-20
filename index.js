84744c9bc3af8ae31b1d1e1026f8c00fbf87c25f
cshif <5175166+cshif@users.noreply.github.com>
2024-06-20 14:09:34 +0800

    feat: add products CRUD APIs

--- index.js
+++ index.js
@@ -113,6 +113,104 @@ app.delete('/users/:id', async (req, res) => {
   }
 });
 
+app.post('/products', async (req, res) => {
+  try {
+    const products = await fs.readFile('./products.json', {
+      encoding: 'utf8',
+    });
+    const newProduct = {
+      id: crypto.randomUUID(),
+      name: req.body.name,
+      price: req.body.price,
+    };
+    await fs.writeFile(
+      './products.json',
+      JSON.stringify([...JSON.parse(products), newProduct])
+    );
+    res.json(newProduct);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await fs.readFile('./products.json', {
+      encoding: 'utf8',
+    });
+    res.json(JSON.parse(products));
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const products = await fs.readFile('./products.json', {
+      encoding: 'utf8',
+    });
+    const product = JSON.parse(products).find(
+      (product) => product.id === req.params.id
+    );
+    res.json(product);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.put('/products/:id', async (req, res) => {
+  try {
+    const products = await fs.readFile('./products.json', {
+      encoding: 'utf8',
+    });
+    const product = JSON.parse(products).find(
+      (product) => product.id === req.params.id
+    );
+    const productIndex = JSON.parse(products).findIndex(
+      (product) => product.id === req.params.id
+    );
+    if (product) {
+      const updatedProduct = {
+        id: product.id,
+        name: req.body.name || product.name,
+        price: req.body.price || product.price,
+      };
+      const copyProducts = [...JSON.parse(products)];
+      copyProducts.splice(productIndex, 1, updatedProduct);
+      await fs.writeFile('./products.json', JSON.stringify(copyProducts));
+      res.json(updatedProduct);
+    } else {
+      res.status(404).send("Can't find product");
+    }
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.delete('/products/:id', async (req, res) => {
+  try {
+    const products = await fs.readFile('./products.json', {
+      encoding: 'utf8',
+    });
+    const productId = JSON.parse(products).find(
+      (product) => product.id === req.params.id
+    )?.id;
+    if (productId) {
+      await fs.writeFile(
+        './products.json',
+        JSON.stringify([
+          ...JSON.parse(products).filter((product) => product.id !== productId),
+        ])
+      );
+      res.json({ id: productId });
+    } else {
+      res.status(404).send("Can't find product");
+    }
+  } catch (e) {
+    console.error(e);
+  }
+});
+
 const PORT = 3009;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
