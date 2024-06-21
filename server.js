c11b8979dedf08ed55d37f303dbad32946522cc7
cshif <5175166+cshif@users.noreply.github.com>
2024-06-21 14:57:11 +0800

    feat: add unhandledRejection

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import app from './app.js';
 
 const PORT = 3009;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.error('Unhandled rejection, shutting down...', {
+    name: error.name,
+    message: error.message,
+  });
+  server.close(() => {
+    process.exit(1);
+  });
 });
