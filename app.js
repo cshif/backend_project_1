584ee092b137d919e8414463e172f61b28132283
cshif <5175166+cshif@users.noreply.github.com>
2024-06-21 15:49:13 +0800

    feat: add uncaughtException; move unhandledRejection error before app initialization

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ import globalErrorHandler from './middlewares/globalErrorHandler.js';
 import userRouter from './routes/userRoutes.js';
 import productRouter from './routes/productRoutes.js';
 
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught exception, shutting down...', {
+    name: error.name,
+    message: error.message,
+  });
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.error('Unhandled rejection, shutting down...', {
+    name: error.name,
+    message: error.message,
+  });
+  process.exit(1);
+});
+
 const app = express();
 app.use(morgan('tiny'));
 app.use(express.json());
--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 import app from './app.js';
 
 const PORT = 3009;
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
-});
-
-process.on('unhandledRejection', (error) => {
-  console.error('Unhandled rejection, shutting down...', {
-    name: error.name,
-    message: error.message,
-  });
-  server.close(() => {
-    process.exit(1);
-  });
 });
