624e01827b8ebd7f8264352e84f82148d7c3a77f
cshif <5175166+cshif@users.noreply.github.com>
2024-06-21 14:01:23 +0800

    refactor: add catchAsync

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,86 +1,67 @@
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
+import catchAsync from '../utils/catchAsync.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const products = JSON.parse(fs.readFileSync(`${__dirname}/../products.json`));
 
-export const createProduct = async (req, res) => {
-  try {
-    const newProduct = {
-      id: crypto.randomUUID(),
-      name: req.body.name,
-      price: req.body.price,
+export const createProduct = catchAsync(async (req, res, next) => {
+  const newProduct = {
+    id: crypto.randomUUID(),
+    name: req.body.name,
+    price: req.body.price,
+  };
+  await fs.writeFileSync(
+    './products.json',
+    JSON.stringify([...products, newProduct])
+  );
+  res.json(newProduct);
+});
+
+export const getProducts = catchAsync(async (req, res, next) => {
+  res.json(products);
+});
+
+export const getProduct = catchAsync(async (req, res, next) => {
+  const product = products.find((product) => product.id === req.params.id);
+  res.json(product);
+});
+
+export const updateProduct = catchAsync(async (req, res, next) => {
+  const product = products.find((product) => product.id === req.params.id);
+  const productIndex = products.findIndex(
+    (product) => product.id === req.params.id
+  );
+  if (product) {
+    const updatedProduct = {
+      id: product.id,
+      name: req.body.name || product.name,
+      price: req.body.price || product.price,
     };
+    const copyProducts = [...products];
+    copyProducts.splice(productIndex, 1, updatedProduct);
+    await fs.writeFileSync('./products.json', JSON.stringify(copyProducts));
+    res.json(updatedProduct);
+  } else {
+    res.status(404).send("Can't find product");
+  }
+});
+
+export const deleteProduct = catchAsync(async (req, res, next) => {
+  const productId = products.find(
+    (product) => product.id === req.params.id
+  )?.id;
+  if (productId) {
     await fs.writeFileSync(
       './products.json',
-      JSON.stringify([...products, newProduct])
+      JSON.stringify([
+        ...products.filter((product) => product.id !== productId),
+      ])
     );
-    res.json(newProduct);
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
+    res.json({ id: productId });
+  } else {
+    res.status(404).send("Can't find product");
   }
-};
-
-export const getProducts = async (req, res) => {
-  try {
-    res.json(products);
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
-  }
-};
-
-export const getProduct = async (req, res) => {
-  try {
-    const product = products.find((product) => product.id === req.params.id);
-    res.json(product);
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
-  }
-};
-
-export const updateProduct = async (req, res) => {
-  try {
-    const product = products.find((product) => product.id === req.params.id);
-    const productIndex = products.findIndex(
-      (product) => product.id === req.params.id
-    );
-    if (product) {
-      const updatedProduct = {
-        id: product.id,
-        name: req.body.name || product.name,
-        price: req.body.price || product.price,
-      };
-      const copyProducts = [...products];
-      copyProducts.splice(productIndex, 1, updatedProduct);
-      await fs.writeFileSync('./products.json', JSON.stringify(copyProducts));
-      res.json(updatedProduct);
-    } else {
-      res.status(404).send("Can't find product");
-    }
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
-  }
-};
-
-export const deleteProduct = async (req, res) => {
-  try {
-    const productId = products.find(
-      (product) => product.id === req.params.id
-    )?.id;
-    if (productId) {
-      await fs.writeFileSync(
-        './products.json',
-        JSON.stringify([
-          ...products.filter((product) => product.id !== productId),
-        ])
-      );
-      res.json({ id: productId });
-    } else {
-      res.status(404).send("Can't find product");
-    }
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
-  }
-};
+});
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,89 +1,67 @@
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
+import catchAsync from '../utils/catchAsync.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const users = JSON.parse(fs.readFileSync(`${__dirname}/../users.json`));
 
-export const createUser = async (req, res) => {
-  try {
-    // 可以寫入 users 的條件：有 email，且 email 沒有使用過
-    if (
-      typeof req.body === 'object' &&
-      Object.keys(req.body).includes('email') &&
-      !users.filter((user) => user.email === req.body.email).length
-    ) {
-      const newUser = {
-        id: crypto.randomUUID(),
-        name: req.body.name,
-        email: req.body.email,
-      };
-      await fs.writeFileSync(
-        './users.json',
-        JSON.stringify([...users, newUser])
-      );
-      res.json(newUser);
-    } else {
-      res.status(404).send('Invalid data.');
-    }
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
+export const createUser = catchAsync(async (req, res, next) => {
+  // 可以寫入 users 的條件：有 email，且 email 沒有使用過
+  if (
+    typeof req.body === 'object' &&
+    Object.keys(req.body).includes('email') &&
+    !users.filter((user) => user.email === req.body.email).length
+  ) {
+    const newUser = {
+      id: crypto.randomUUID(),
+      name: req.body.name,
+      email: req.body.email,
+    };
+    await fs.writeFileSync('./users.json', JSON.stringify([...users, newUser]));
+    res.json(newUser);
+  } else {
+    res.status(404).send('Invalid data.');
   }
-};
+});
 
-export const getUsers = async (req, res) => {
-  try {
-    res.json(users);
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
-  }
-};
+export const getUsers = catchAsync(async (req, res, next) => {
+  res.json(users);
+});
 
-export const getUser = async (req, res) => {
-  try {
-    const user = users.find((user) => user.id === req.params.id);
-    res.json(user);
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
-  }
-};
+export const getUser = catchAsync(async (req, res, next) => {
+  const user = users.find((user) => user.id === req.params.id);
+  res.json(user);
+});
 
-export const updateUser = async (req, res) => {
-  try {
-    const user = users.find((user) => user.id === req.params.id);
-    const userIndex = users.findIndex((user) => user.id === req.params.id);
-    if (user) {
-      const updatedUser = {
-        id: user.id,
-        name: req.body.name || user.name,
-        email: req.body.email || user.email,
-      };
-      const copyUsers = [...users];
-      copyUsers.splice(userIndex, 1, updatedUser);
-      await fs.writeFileSync('./users.json', JSON.stringify(copyUsers));
-      res.json(updatedUser);
-    } else {
-      res.status(404).send("Can't find user");
-    }
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
+export const updateUser = catchAsync(async (req, res, next) => {
+  const user = users.find((user) => user.id === req.params.id);
+  const userIndex = users.findIndex((user) => user.id === req.params.id);
+  if (user) {
+    const updatedUser = {
+      id: user.id,
+      name: req.body.name || user.name,
+      email: req.body.email || user.email,
+    };
+    const copyUsers = [...users];
+    copyUsers.splice(userIndex, 1, updatedUser);
+    await fs.writeFileSync('./users.json', JSON.stringify(copyUsers));
+    res.json(updatedUser);
+  } else {
+    res.status(404).send("Can't find user");
   }
-};
+});
 
-export const deleteUser = async (req, res) => {
-  try {
-    const userId = users.find((user) => user.id === req.params.id)?.id;
-    if (userId) {
-      await fs.writeFileSync(
-        './users.json',
-        JSON.stringify([...users.filter((user) => user.id !== userId)])
-      );
-      res.json({ id: userId });
-    } else {
-      res.status(404).send("Can't find user");
-    }
-  } catch (e) {
-    res.status(500).send(new Error(e.message));
+export const deleteUser = catchAsync(async (req, res, next) => {
+  const userId = users.find((user) => user.id === req.params.id)?.id;
+  if (userId) {
+    await fs.writeFileSync(
+      './users.json',
+      JSON.stringify([...users.filter((user) => user.id !== userId)])
+    );
+    res.json({ id: userId });
+  } else {
+    res.status(404).send("Can't find user");
   }
-};
+});
