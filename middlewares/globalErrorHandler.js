a0aeb593fd360ac719b450d950a0772cb46d9e5b
cshif <5175166+cshif@users.noreply.github.com>
2024-06-21 14:32:10 +0800

    feat: send different error for development and production environment

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -1,8 +1,33 @@
-export default (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendErrorProd = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+export default (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    if (err.isOperational) {
+      sendErrorProd(err, res);
+    } else {
+      console.error('💥');
+      res.status(500).json({
+        status: 'error',
+        message: 'OMG! WHAT DID THEY DO?',
+      });
+    }
+  }
 };
