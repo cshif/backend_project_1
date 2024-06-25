d2e99f6914ca4ca886235a286a3ef8139c0012c9
cshif <5175166+cshif@users.noreply.github.com>
2024-06-25 17:03:59 +0800

    feat: send token when user successfully registered

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import * as db from '../db/index.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import catchAsync from '../utils/catchAsync.js';
 import AppError from '../core/AppError.js';
 
@@ -11,8 +13,17 @@ export const register = catchAsync(async (req, res, next) => {
 
   const hashedPassword = await bcrypt.hash(password, 12);
   const { rows } = await db.query(
-    `INSERT INTO users(email, password) VALUES ($1,$2) RETURNING *`,
+    `INSERT INTO users(email, password) VALUES ($1,$2) RETURNING id, email`,
     [email, hashedPassword]
   );
-  res.json(rows[0]);
+
+  const token = await jwt.sign({ id: rows[0].id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: rows[0],
+    token,
+  });
 });
