93a58b9ea9eaf5f25d1d407d1bb1a0a923a5f167
cshif <5175166+cshif@users.noreply.github.com>
2024-07-08 11:09:05 +0800

    feat: modify product controllers - rename table name - required fields when creating

--- src/modules/product/presentation/product.controllers.js
+++ src/modules/product/presentation/product.controllers.js
@@ -4,20 +4,23 @@ import * as db from '../../../config/db/index.js';
 
 export const createProduct = catchAsync(async (req, res, next) => {
   const { name, price } = req.body;
+  if (!name || !price) {
+    return next(new AppError('Lack of required data', 404));
+  }
   const { rows } = await db.query(
-    `INSERT INTO products(name, price) VALUES ($1,$2) RETURNING id, name`,
+    `INSERT INTO "Product"(name, price) VALUES ($1,$2) RETURNING *`,
     [name, price]
   );
   res.json(rows[0]);
 });
 
 export const getProducts = catchAsync(async (req, res, next) => {
-  const { rows } = await db.query(`SELECT * FROM products`);
+  const { rows } = await db.query(`SELECT * FROM "Product"`);
   res.json({ rows });
 });
 
 export const getProduct = catchAsync(async (req, res, next) => {
-  const { rows } = await db.query(`SELECT * FROM products WHERE id = $1`, [
+  const { rows } = await db.query(`SELECT * FROM "Product" WHERE id = $1`, [
     req.params.id,
   ]);
 
@@ -33,7 +36,7 @@ export const updateProduct = catchAsync(async (req, res, next) => {
   const productId = Number(req.params.id);
 
   const { rows: products } = await db.query(
-    `SELECT * FROM products WHERE id = $1`,
+    `SELECT * FROM "Product" WHERE id = $1`,
     [req.params.id]
   );
 
@@ -42,7 +45,7 @@ export const updateProduct = catchAsync(async (req, res, next) => {
   }
 
   const { rows } = await db.query(
-    `UPDATE products 
+    `UPDATE "Product" 
      SET name  = CASE WHEN COALESCE($1) IS NOT NULL THEN $1 ELSE name END,
          price = CASE WHEN COALESCE($2::numeric) IS NOT NULL THEN $2 ELSE price END
      WHERE id = $3 
@@ -56,7 +59,7 @@ export const updateProduct = catchAsync(async (req, res, next) => {
 export const deleteProduct = catchAsync(async (req, res, next) => {
   const productId = Number(req.params.id);
   const { rows: products } = await db.query(
-    `SELECT * FROM products WHERE id = $1`,
+    `SELECT * FROM "Product" WHERE id = $1`,
     [req.params.id]
   );
 
@@ -65,7 +68,7 @@ export const deleteProduct = catchAsync(async (req, res, next) => {
   }
 
   const { rows } = await db.query(
-    `DELETE FROM products WHERE id = $1 RETURNING id`,
+    `DELETE FROM "Product" WHERE id = $1 RETURNING *`,
     [productId]
   );
 
