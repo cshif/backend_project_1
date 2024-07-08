6066f147dabf2c05965afd6fca01e37debe35c2c
cshif <5175166+cshif@users.noreply.github.com>
2024-07-08 10:48:29 +0800

    feat: modify user controllers - rename table name - add currentDeviceId column - alter column roleId to role

--- src/modules/user/presentation/user.controllers.js
+++ src/modules/user/presentation/user.controllers.js
@@ -5,7 +5,8 @@ import { AppError } from '../../../common/classes/index.js';
 
 export const createUser = catchAsync(async (req, res, next) => {
   // 可以寫入 users 的條件：有 email，且 email 沒有使用過
-  const { name, email, password, lang, roleId, avatarURL } = req.body;
+  const { name, email, password, lang, role, avatarURL, currentDeviceId } =
+    req.body;
   if (!email || !password) {
     return next(new AppError('Lack of required data', 404));
   }
@@ -14,22 +15,22 @@ export const createUser = catchAsync(async (req, res, next) => {
 
   const hashedPassword = await bcrypt.hash(password, 12);
   const { rows } = await db.query(
-    `INSERT INTO users(
-      name, email, password, lang, "roleId", "avatarURL"
-    ) VALUES ($1,$2,$3,$4,$5,$6) 
+    `INSERT INTO "User"(
+      name, email, password, lang, role, "avatarURL", "currentDeviceId"
+    ) VALUES ($1,$2,$3,$4,$5,$6,$7) 
     RETURNING *`,
-    [name, email, hashedPassword, lang, roleId, avatarURL]
+    [name, email, hashedPassword, lang, role, avatarURL, currentDeviceId]
   );
   res.json(rows[0]);
 });
 
 export const getUsers = catchAsync(async (req, res, next) => {
-  const { rows } = await db.query(`SELECT * FROM users`);
+  const { rows } = await db.query(`SELECT * FROM "User"`);
   res.json({ rows });
 });
 
 export const getUser = catchAsync(async (req, res, next) => {
-  const { rows } = await db.query(`SELECT * FROM users WHERE id = $1`, [
+  const { rows } = await db.query(`SELECT * FROM "User" WHERE id = $1`, [
     Number(req.params.id),
   ]);
 
@@ -41,10 +42,10 @@ export const getUser = catchAsync(async (req, res, next) => {
 });
 
 export const updateUser = catchAsync(async (req, res, next) => {
-  const { name, password, lang, roleId, avatarURL } = req.body;
+  const { name, password, lang, role, avatarURL, currentDeviceId } = req.body;
   const userId = Number(req.params.id);
 
-  const { rows: users } = await db.query(`SELECT * FROM users WHERE id = $1`, [
+  const { rows: users } = await db.query(`SELECT * FROM "User" WHERE id = $1`, [
     userId,
   ]);
 
@@ -54,15 +55,16 @@ export const updateUser = catchAsync(async (req, res, next) => {
 
   const newHashedPassword = await bcrypt.hash(password, 12);
   const { rows } = await db.query(
-    `UPDATE users
-       SET name        = CASE WHEN COALESCE($1) IS NOT NULL THEN $1 ELSE name END,
-           password    = CASE WHEN COALESCE($2) IS NOT NULL THEN $2 ELSE password END,
-           lang        = CASE WHEN COALESCE($3) IS NOT NULL THEN $3 ELSE lang END,
-           "roleId"    = CASE WHEN COALESCE($4::int) IS NOT NULL THEN $4 ELSE "roleId" END,
-           "avatarURL" = CASE WHEN COALESCE($5) IS NOT NULL THEN $5 ELSE "avatarURL" END
-       WHERE id = $6
-       RETURNING *`,
-    [name, newHashedPassword, lang, roleId, avatarURL, userId]
+    `UPDATE "User"
+       SET name              = CASE WHEN COALESCE($1) IS NOT NULL THEN $1 ELSE name END,
+           password          = CASE WHEN COALESCE($2) IS NOT NULL THEN $2 ELSE password END,
+           lang              = CASE WHEN COALESCE($3) IS NOT NULL THEN $3 ELSE lang END,
+           role              = CASE WHEN COALESCE($4::"Role") IS NOT NULL THEN $4 ELSE role END,
+           "avatarURL"       = CASE WHEN COALESCE($5) IS NOT NULL THEN $5 ELSE "avatarURL" END,
+           "currentDeviceId" = CASE WHEN COALESCE($6::BIGINT) IS NOT NULL THEN $6 ELSE "currentDeviceId" END
+       WHERE id = $7
+     RETURNING *`,
+    [name, newHashedPassword, lang, role, avatarURL, currentDeviceId, userId]
   );
   res.json(rows[0]);
 });
@@ -70,7 +72,7 @@ export const updateUser = catchAsync(async (req, res, next) => {
 export const deleteUser = catchAsync(async (req, res, next) => {
   const userId = Number(req.params.id);
 
-  const { rows: users } = await db.query(`SELECT * FROM users WHERE id = $1`, [
+  const { rows: users } = await db.query(`SELECT * FROM "User" WHERE id = $1`, [
     userId,
   ]);
 
@@ -79,7 +81,7 @@ export const deleteUser = catchAsync(async (req, res, next) => {
   }
 
   const { rows } = await db.query(
-    `DELETE FROM users WHERE id = $1 RETURNING id`,
+    `DELETE FROM "User" WHERE id = $1 RETURNING *`,
     [userId]
   );
 
