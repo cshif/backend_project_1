7ffd9a89da238842da8c72f1d14d4a2da34767d6
cshif <5175166+cshif@users.noreply.github.com>
2024-07-09 13:45:26 +0800

    feat: replace product-related SQL syntax with Prisma Client model queries

---
+++ src/modules/product/application/product.service.js
@@ -0,0 +1,69 @@
+import { PrismaClient } from '@prisma/client';
+import filterNullValues from '../../../common/utils/filterNullValues.js';
+import bigIntReplacer from '../../../common/utils/bigIntReplacer.js';
+
+const prisma = new PrismaClient();
+
+const ProductService = {
+  createProduct: async (req, res, next) => {
+    const { name, price } = req.body;
+
+    try {
+      const row = await prisma.product.create({
+        data: filterNullValues({
+          name,
+          price,
+        }),
+      });
+      return JSON.parse(JSON.stringify(row, bigIntReplacer));
+    } catch (e) {
+      return next(e);
+    }
+  },
+  getProducts: async (req, res, next) => {
+    try {
+      const rows = await prisma.product.findMany();
+      return JSON.parse(JSON.stringify(rows, bigIntReplacer));
+    } catch (e) {
+      return next(e);
+    }
+  },
+  getProductById: async (req, res, next) => {
+    try {
+      const row = await prisma.product.findUniqueOrThrow({
+        where: { id: req.params.id },
+      });
+      return JSON.parse(JSON.stringify(row, bigIntReplacer));
+    } catch (e) {
+      return next(e);
+    }
+  },
+  updateProduct: async (req, res, next) => {
+    const { name, price } = req.body;
+
+    try {
+      const row = await prisma.product.update({
+        where: { id: req.params.id },
+        data: filterNullValues({
+          name,
+          price,
+        }),
+      });
+      return JSON.parse(JSON.stringify(row, bigIntReplacer));
+    } catch (e) {
+      return next(e);
+    }
+  },
+  deleteProduct: async (req, res, next) => {
+    try {
+      const row = await prisma.product.delete({
+        where: { id: req.params.id },
+      });
+      return JSON.parse(JSON.stringify(row, bigIntReplacer));
+    } catch (e) {
+      return next(e);
+    }
+  },
+};
+
+export default ProductService;
--- src/modules/product/presentation/product.controllers.js
+++ src/modules/product/presentation/product.controllers.js
@@ -1,76 +1,44 @@
 import catchAsync from '../../../common/utils/catchAsync.js';
 import { AppError } from '../../../common/classes/index.js';
-import * as db from '../../../config/db/index.js';
+import ProductService from '../application/product.service.js';
 
 export const createProduct = catchAsync(async (req, res, next) => {
   const { name, price } = req.body;
   if (!name || !price) {
     return next(new AppError('Lack of required data', 404));
   }
-  const { rows } = await db.query(
-    `INSERT INTO "Product"(name, price) VALUES ($1,$2) RETURNING *`,
-    [name, price]
-  );
-  res.json(rows[0]);
+  const row = await ProductService.createProduct(req, res, next);
+  res.json(row);
 });
 
 export const getProducts = catchAsync(async (req, res, next) => {
-  const { rows } = await db.query(`SELECT * FROM "Product"`);
-  res.json({ rows });
+  const row = await ProductService.getProducts(req, res, next);
+  res.json(row);
 });
 
 export const getProduct = catchAsync(async (req, res, next) => {
-  const { rows } = await db.query(`SELECT * FROM "Product" WHERE id = $1`, [
-    req.params.id,
-  ]);
+  const row = await ProductService.getProductById(req, res, next);
 
-  if (!rows.length) {
-    return next(new AppError("Can't find the product", 404));
-  }
+  if (!row) return next();
 
-  res.json(rows[0]);
+  res.json(row);
 });
 
 export const updateProduct = catchAsync(async (req, res, next) => {
-  const { name, price } = req.body;
-  const productId = Number(req.params.id);
+  const row = await ProductService.getProductById(req, res, next);
 
-  const { rows: products } = await db.query(
-    `SELECT * FROM "Product" WHERE id = $1`,
-    [req.params.id]
-  );
+  if (!row) return next();
 
-  if (!products.length) {
-    return next(new AppError("Can't find the product", 404));
-  }
-
-  const { rows } = await db.query(
-    `UPDATE "Product" 
-     SET name  = CASE WHEN COALESCE($1) IS NOT NULL THEN $1 ELSE name END,
-         price = CASE WHEN COALESCE($2::numeric) IS NOT NULL THEN $2 ELSE price END
-     WHERE id = $3 
-     RETURNING *
-    `,
-    [name, price, productId]
-  );
-  res.json(rows[0]);
+  const product = await ProductService.updateProduct(req, res, next);
+  res.json(product);
 });
 
 export const deleteProduct = catchAsync(async (req, res, next) => {
-  const productId = Number(req.params.id);
-  const { rows: products } = await db.query(
-    `SELECT * FROM "Product" WHERE id = $1`,
-    [req.params.id]
-  );
+  const row = await ProductService.getProductById(req, res, next);
 
-  if (!products.length) {
-    return next(new AppError("Can't find the product", 404));
-  }
+  if (!row) return next();
 
-  const { rows } = await db.query(
-    `DELETE FROM "Product" WHERE id = $1 RETURNING *`,
-    [productId]
-  );
+  const product = await ProductService.deleteProduct(req, res, next);
 
-  res.json(rows[0]);
+  res.json(product);
 });
