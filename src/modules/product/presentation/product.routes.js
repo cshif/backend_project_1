64e222b39fbd3aeba101c55ed88ef108ecd2348c
cshif <5175166+cshif@users.noreply.github.com>
2024-07-29 18:39:49 +0800

    feat: comment unmodified routes

--- src/modules/product/presentation/product.routes.js
+++ src/modules/product/presentation/product.routes.js
@@ -1,29 +1,29 @@
 import express from 'express';
-import * as productControllers from './product.controllers.js';
-import * as authControllers from '../../auth/presentation/auth.controllers.js';
-import roles from '../../../constants/roles.js';
+// import * as productControllers from './product.controllers.js';
+// import * as authControllers from '../../auth/presentation/auth.controllers.js';
+// import roles from '../../../constants/roles.js';
 
 const router = express.Router();
 
-router
-  .route('/')
-  .all(authControllers.protectRoute)
-  .post(
-    authControllers.restrictTo(roles.ADMIN, roles.SALES),
-    productControllers.createProduct
-  )
-  .get(productControllers.getProducts);
-router
-  .route('/:id')
-  .all(authControllers.protectRoute)
-  .get(productControllers.getProduct)
-  .put(
-    authControllers.restrictTo(roles.ADMIN, roles.SALES),
-    productControllers.updateProduct
-  )
-  .delete(
-    authControllers.restrictTo(roles.ADMIN),
-    productControllers.deleteProduct
-  );
+// router
+//   .route('/')
+//   .all(authControllers.protectRoute)
+//   .post(
+//     authControllers.restrictTo(roles.ADMIN, roles.SALES),
+//     productControllers.createProduct
+//   )
+//   .get(productControllers.getProducts);
+// router
+//   .route('/:id')
+//   .all(authControllers.protectRoute)
+//   .get(productControllers.getProduct)
+//   .put(
+//     authControllers.restrictTo(roles.ADMIN, roles.SALES),
+//     productControllers.updateProduct
+//   )
+//   .delete(
+//     authControllers.restrictTo(roles.ADMIN),
+//     productControllers.deleteProduct
+//   );
 
 export default router;
