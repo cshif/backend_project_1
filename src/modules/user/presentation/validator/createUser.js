0c8025e44befb73ff1a31fbf2975c1594f2cbd5d
cshif <5175166+cshif@users.noreply.github.com>
2024-08-02 01:08:09 +0800

    feat: handle unknown fields input from client

--- src/modules/user/presentation/validator/createUser.js
+++ src/modules/user/presentation/validator/createUser.js
@@ -1,78 +1,92 @@
 import inspect from '../../../../devtools/inspect.js';
-import { checkSchema } from 'express-validator';
+import { checkSchema, checkExact, validationResult } from 'express-validator';
 import joi from 'joi';
 import roles from '../../../../constants/roles.js';
 import { AppError } from '../../../../common/classes/index.js';
 
 const createUserValidatorByExpressValidator = async (req, res, next) => {
-  const result = await checkSchema(
-    {
-      email: {
-        isEmail: { errorMessage: 'Please provide valid email' },
-      },
-      password: {
-        isLength: {
-          options: { min: 8 },
-          errorMessage: 'Please provide valid password',
+  await checkExact(
+    checkSchema(
+      {
+        email: {
+          isEmail: { errorMessage: 'Please provide valid email' },
         },
-        custom: {
-          options: async (value) => {
-            if (!value.startsWith('A')) throw new Error('should start with A');
-            return true;
+        password: {
+          isLength: {
+            options: { min: 8 },
+            errorMessage: 'Please provide valid password',
+          },
+          custom: {
+            options: async (value) => {
+              if (!value.startsWith('A'))
+                throw new Error('should start with A');
+              return true;
+            },
           },
         },
-      },
-      role: {
-        isIn: {
-          options: [Object.values(roles)],
-          errorMessage: `should be one of ${Object.values(roles).join(', ')}`,
+        role: {
+          isIn: {
+            options: [Object.values(roles)],
+            errorMessage: `should be one of ${Object.values(roles).join(', ')}`,
+          },
+          optional: true,
         },
-        optional: true,
+        avatarURL: {
+          isURL: true,
+          optional: true,
+        },
       },
-      avatarURL: {
-        isURL: true,
-        optional: true,
+      ['body']
+    ),
+    {
+      locations: ['body'],
+      message: ([field]) => {
+        return `Unknown field ${field.path} in ${field.location} with value ${field.value}`;
       },
-    },
-    ['body']
+    }
   ).run(req);
 
-  const errors = result.reduce((all, field) => [...all, ...field.errors], []);
+  const { errors } = validationResult(req);
 
-  if (errors.length) {
-    inspect(JSON.parse(JSON.stringify(errors, null, 2)));
+  if (errors.filter((error) => error.type === 'unknown_fields').length) {
+    inspect(errors);
+    return next(new AppError('do not send unknown fields', 401));
+  } else if (errors.filter((error) => error.type !== 'unknown_fields').length) {
+    inspect(errors);
     return next(new AppError('the error messages', 401));
   }
   return next();
 };
 
 const createUserValidatorByJoi = async (req, res, next) => {
-  const schema = joi.object({
-    email: joi.string().email().required().messages({
-      'string.email': 'Please provide valid email',
-    }),
-    password: joi
-      .string()
-      .length(8)
-      .custom((value, helpers) => {
-        const capital = 'A';
-        return value.startsWith(capital)
-          ? value
-          : helpers.error('custom.startsWith', { capital });
-      })
-      .required()
-      .messages({
-        'string.length': 'Please provide valid password',
-        'custom.startsWith': 'should start with {#capital}',
+  const schema = joi
+    .object({
+      email: joi.string().email().required().messages({
+        'string.email': 'Please provide valid email',
       }),
-    role: joi
-      .string()
-      .valid(...Object.values(roles))
-      .messages({
-        'any.only': `should be one of ${Object.values(roles).join(', ')}`,
-      }),
-    avatarURL: joi.string().uri(),
-  });
+      password: joi
+        .string()
+        .length(8)
+        .custom((value, helpers) => {
+          const capital = 'A';
+          return value.startsWith(capital)
+            ? value
+            : helpers.error('custom.startsWith', { capital });
+        })
+        .required()
+        .messages({
+          'string.length': 'Please provide valid password',
+          'custom.startsWith': 'should start with {#capital}',
+        }),
+      role: joi
+        .string()
+        .valid(...Object.values(roles))
+        .messages({
+          'any.only': `should be one of ${Object.values(roles).join(', ')}`,
+        }),
+      avatarURL: joi.string().uri(),
+    })
+    .unknown(false);
 
   try {
     await schema.validateAsync(req.body, { abortEarly: false });
